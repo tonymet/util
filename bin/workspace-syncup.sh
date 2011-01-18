1c53679e78df029b22d9fe26b4fc3aba198258cb
Tony Metzidis <tonym@tonym.us>
2011-01-18 14:25:25 -0800

    use zla dev host, support rsync.conf and enable dryrun

--- bin/workspace-syncup.sh
+++ bin/workspace-syncup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-RSYNC_COMMAND="rsync -zrltgoCD  --delete --include core"
-REMOTE_HOST="laughterniece-vm1.santamonica.corp.yahoo.com"
+RSYNC_OPTS="-zrltgoCD"
+if [[ $1 == "-n" ]];then
+	RSYNC_OPTS="$RSYNC_OPTS"nP
+	echo "DRY RUN ENABLED"
+fi
+RSYNC_COMMAND="rsync $RSYNC_OPTS --delete --include core"
+# recognize a rsync.conf in the current directory
+if [[ -f rsync.conf ]]; then
+	RSYNC_COMMAND="rsync $RSYNC_OPTS --exclude-from=rsync.conf --delete --include core"
+	echo "excluding files using rsync.conf"
+fi
+REMOTE_HOST=$zdev1
+echo "syncing files to $REMOTE_HOST"
 WORKSPACE=\~/workspace
 PROJECT=`basename $(pwd)`
 
